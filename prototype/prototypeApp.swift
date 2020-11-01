93ac08c72cd7b00205a16ef53bbe6897fb7b75f5
pianostringquartet <cjc500@nyu.edu>
2020-11-01 14:58:09 -0700

    Initial Commit

---
+++ prototype/prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  prototypeApp.swift
+//  prototype
+//
+//  Created by cjc on 11/1/20.
+//
+
+import SwiftUI
+
+@main
+struct prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
