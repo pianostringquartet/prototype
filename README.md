0bdb0900e1d6a2339610b44a05f7d7b18805b63e
pianostringquartet <cjc500@nyu.edu>
2020-11-01 20:26:50 -0700

    Arbitrary edges for movable nodes

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 Prototype.
 
+## Stage 1: movable nodes with arbitrary edges
+
+Every ball can be moved around and have arbitrary connections with other balls.
+
+![](prototype_edges.png)
+![](prototype_edges_2.png)
+![](prototype_edges_3.png)
+![](prototype_edges_4.png)
+![](prototype_edges_5.png)
+
+
+## Stage 0: movable nodes
+
 Black ball moves around, bounces back to original position.
 Other balls will stay where dragged.
 
--- prototype/ContentView.swift
+++ prototype/ContentView.swift
@@ -7,6 +7,43 @@
 
 import SwiftUI
 
+/* ----------------------------------------------------------------
+ PREFERENCE DATA: passing data up from children to parent view
+ ---------------------------------------------------------------- */
+
+// Datatype for preference data
+struct BallPreferenceData {
+    let viewIdx: Int
+    let center: Anchor<CGPoint>
+}
+
+// Preference key for preference data
+struct BallPreferenceKey: PreferenceKey {
+    typealias Value = [BallPreferenceData]
+    
+    static var defaultValue: [BallPreferenceData] = []
+    
+    static func reduce(value: inout [BallPreferenceData], nextValue: () -> [BallPreferenceData]) {
+        value.append(contentsOf: nextValue())
+    }
+    
+}
+
+
+/* ----------------------------------------------------------------
+ UI ELEMENTS: draggable balls, etc.
+ ---------------------------------------------------------------- */
+
+struct Line: Shape {
+    let from, to: CGPoint
+    func path(in rect: CGRect) -> Path {
+        Path { p in
+            p.move(to: self.from)
+            p.addLine(to: self.to)
+        }
+    }
+}
+
 func coloredCircle(color: Color, radius: CGFloat) -> some View {
     LinearGradient(gradient: Gradient(colors: [.white, color]),
                    startPoint: .topLeading,
@@ -20,69 +57,74 @@ func updatePosition(value: DragGesture.Value, position: CGSize) -> CGSize {
     CGSize(width: value.translation.width + position.width,
            height: value.translation.height + position.height)
 }
-    
-// A ball that stays where you drag it
-struct DraggableBall: View {
-    
-    // how far displaced in given gesture
+ 
+struct EdgeBall: View {
     @State private var position = CGSize.zero
-    
     @State private var previousPosition = CGSize.zero
-        
+    
+    let idx: Int
     let color: Color
     let radius: CGFloat
     
     var body: some View {
         coloredCircle(color: color, radius: radius)
-            
-            // move ball as we drag
+            // Child stores its center in anchor preference data,
+            // for parent to later access.
+            // NOTE: must come before .offset modifier
+            .anchorPreference(key: BallPreferenceKey.self,
+                              value: .center, // center for Anchor<CGPoint>
+                              transform: { [BallPreferenceData(viewIdx: self.idx, center: $0)] })
             .offset(x: self.position.width, y: self.position.height)
-            
-            // alternatively: move ball only after we let go
-//            .offset(x: self.previousPosition.width, y: self.previousPosition.height)
-            
             .gesture(DragGesture()
                         .onChanged { self.position = updatePosition(value: $0, position: self.previousPosition) }
                         .onEnded { _ in self.previousPosition = self.position })
-            
-            // give ball some bounce
             .animation(.spring(response: 0.3, dampingFraction: 0.65, blendDuration: 4))
     }
 }
 
 
-// A ball that bounces back to its original position
-struct BoomerangBall: View {
-    
-    @State private var position = CGSize.zero
-    
-    let color: Color
-    let radius: CGFloat
-    
-    var body: some View {
-        coloredCircle(color: .black, radius: 125)
-        .offset(position)
-        .gesture(DragGesture()
-                    .onChanged { self.position = $0.translation }
-                    .onEnded { _ in
-                        withAnimation(.spring()) { self.position = .zero }
-                    })
-    }
-}
-
+/* ----------------------------------------------------------------
+ CONTENT VIEW
+ ---------------------------------------------------------------- */
 
 struct ContentView: View {
-    
+    let spacing: CGFloat = 100
     var body: some View {
-        VStack {
-            BoomerangBall(color: .black, radius: 125)
-            DraggableBall(color: .red, radius: 100)
-            DraggableBall(color: .blue, radius: 75)
-            DraggableBall(color: .green, radius: 50)
+        VStack (spacing: spacing) {
+            EdgeBall(idx: 0, color: .purple, radius: 75)
+            HStack (spacing: spacing) {
+                EdgeBall(idx: 1, color: .pink, radius: 50)
+                EdgeBall(idx: 2, color: .green, radius: 75)
+            }
+            EdgeBall(idx: 3, color: .blue, radius: 50)
+        }
+        
+        // parent uses preference data to know about balls' relative centers
+        .backgroundPreferenceValue(BallPreferenceKey.self) { preferences in
+            
+            // use GeometryReader to get absolute center of ball
+            GeometryReader { geometry in
+                // TODO: cleaner, programmatic construction of edges
+                let point0 = geometry[preferences[0].center]
+                let point1 = geometry[preferences[1].center]
+                let point2 = geometry[preferences[2].center]
+                let point3 = geometry[preferences[3].center]
+                
+                // Create any arbitrary connection:
+                Line(from: point0, to: point1).stroke().animation(.default)
+                Line(from: point1, to: point2).stroke().animation(.default)
+                Line(from: point2, to: point3).stroke().animation(.default)
+                Line(from: point0, to: point3).stroke().animation(.default)
+                Line(from: point1, to: point3).stroke().animation(.default)
+            }
         }
     }
 }
 
+
+/* ----------------------------------------------------------------
+ PREVIEW
+ ---------------------------------------------------------------- */
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
